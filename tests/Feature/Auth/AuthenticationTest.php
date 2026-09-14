20d9903fc78e4a1a5b83a59ea2274d49ce25629b
Enzo Fagundes <enzo.fagundes014@gmail.com>
2026-09-14 20:04:35 -0300

    test(usuario): cobre bloqueio de login de inativo

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -48,6 +48,22 @@ class AuthenticationTest extends TestCase
         $this->assertGuest();
     }
 
+    public function test_deactivated_users_can_not_authenticate(): void
+    {
+        $user = User::factory()->create();
+        $user->is_active = false;
+        $user->save();
+
+        $response = $this->post(route('login.store'), [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertSessionHasErrorsIn('email');
+
+        $this->assertGuest();
+    }
+
     public function test_users_with_two_factor_enabled_are_redirected_to_two_factor_challenge(): void
     {
         $this->skipUnlessFortifyHas(Features::twoFactorAuthentication());
