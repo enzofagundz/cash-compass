a0d056b4358eb0397bcc705875db10e3f96ff41a
Enzo Fagundes <enzo.fagundes014@gmail.com>
2026-09-15 09:18:00 -0300

    fix(usuario): remove usuário de teste da seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,14 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
-        $admin = User::factory()->admin()->create([
+        User::factory()->admin()->create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
         ]);
