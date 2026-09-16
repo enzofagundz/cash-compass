ba7a3979ce90896bea25c1119de5eda218613d54
Enzo Fagundes <enzo.fagundes014@gmail.com>
2026-09-16 14:33:27 -0300

    feat(painel): carrega tema próprio do Filament

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -35,6 +35,7 @@ class AppPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->viteTheme('resources/css/filament/app/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
